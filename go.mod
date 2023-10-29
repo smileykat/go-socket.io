34abbae4f587a07d69ce546c52865da72a47c986
lam.nguyentung <lam.nguyentung@shopee.com>
2023-10-29 21:04:47 +0800

    update log

--- go.mod
+++ go.mod
@@ -7,5 +7,4 @@ require (
 	github.com/gomodule/redigo v1.8.9
 	github.com/gorilla/websocket v1.5.0
 	github.com/stretchr/testify v1.8.4
-	golang.org/x/exp v0.0.0-20230713183714-613f0c0eb8a1
 )
--- logger/logger.go
+++ logger/logger.go
@@ -1,15 +1,33 @@
 package logger
 
 import (
-	"golang.org/x/exp/slog"
+	"fmt"
+	"log"
+	"strings"
 )
 
-var Log *slog.Logger = slog.Default()
-
 func Error(msg string, err error) {
-	Log.Error(msg, "err", err.Error())
+	//"golang.org/x/exp/slog"
+	//Log.Error(msg, "err", err.Error())
+	log.Printf("[ERROR] %v\n", err)
 }
 
 func Info(msg string, args ...interface{}) {
-	Log.Info(msg, args...)
+	//Log.Info(msg, args...)
+	sb := strings.Builder{}
+	sb.WriteString(msg)
+	if len(args) > 0 {
+		sb.WriteString(": ")
+		for i := 0; i < len(args); i += 2 {
+			if i > 0 {
+				sb.WriteString(", ")
+			}
+			key, val := args[i], interface{}("")
+			if i+1 < len(args) {
+				val = args[i+1]
+			}
+			sb.WriteString(fmt.Sprintf("%v=%v", key, val))
+		}
+	}
+	log.Printf("[INFO] %s\n", sb.String())
 }
